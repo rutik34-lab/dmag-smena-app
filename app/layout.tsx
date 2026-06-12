ae6f1913b1f7c568cf88eef85b6caf5865fb56dd
rutik34-lab <rutik34@gmail.com>
2026-06-12 09:31:01 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'DMAG',
+  description: 'Учет рабочего времени',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ru">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
